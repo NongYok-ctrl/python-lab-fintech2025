93362de9b387f745beffed3fecf72edada048d81
Yok Chanan <chanan3847@gmail.com>
2025-09-20 14:48:50 +0700

    add commit

--- main.py
+++ main.py
@@ -7,6 +7,13 @@ data = pd.read_csv('product.csv')
 st.title("dashboard")
 st.write("this application is about displaying product sales")
 
+name = st.text_input("Enter your name", "Type here ...")
+st.write("name")
+width = st.number_input("width")
+hight = st.number_input("hight")
+if st.button("calculate area"):
+    st.write(width * hight)
+
 col1, col2, col3 = st.columns(3)
 with col1:
     st.metric("sales", "1,200", "12%")
@@ -15,9 +22,10 @@ with col2:
 with col3:
     st.metric("profit", "$ 30,000", "5%")
 
-st.line_chart(data['sales'])
+st.line_chart(data['sales'])\
 
-st.header("sales table")
-st.subheader("this is a sales table")
-st.write(data)
+if st.button("Click me"):
+    st.header("sales table")
+    st.subheader("this is a sales table")
+    st.write(data)
 
